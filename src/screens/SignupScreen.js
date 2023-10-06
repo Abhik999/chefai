a81c732c02b3ab23ed68a5b5ffc1645c9f02a240
Abhik999 <144631044+Abhik999@users.noreply.github.com>
2023-10-05 23:45:23 -0700

    Initial commit

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,56 @@
+import React, { useState, useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import FormButton from '../components/FormButton';
+import FormInput from '../components/FormInput';
+import { AuthContext } from '../navigation/AuthProvider';
+
+export default function SignupScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const { register } = useContext(AuthContext);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Create an account</Text>
+      <FormInput
+        value={email}
+        placeholderText='Email'
+        onChangeText={userEmail => setEmail(userEmail)}
+        autoCapitalize='none'
+        keyboardType='email-address'
+        autoCorrect={false}
+      />
+      <FormInput
+        value={password}
+        placeholderText='Password'
+        onChangeText={userPassword => setPassword(userPassword)}
+        secureTextEntry={true}
+      />
+      <FormInput
+        value={username}
+        placeholderText='UserName'
+        onChangeText={userName => setUsername(userName)}
+        autoCapitalize='none'
+        keyboardType='user-name'
+        autoCorrect={false}
+      />
+      <FormButton
+        buttonTitle='Signup'
+        onPress={() => register(email, password,username)}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 10
+  }
+})
